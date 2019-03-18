c299a23d121da7cad1ab101ab4e9f27ae2957ec7
Maibenben <2298618484@qq.com>
2019-03-18 14:26:38 +0800

    Merge branch 'master' of https://github.com/2298618484/lmr
    # Conflicts:
    #	src/main/java/com/hr/sb5mybatis/controller/test.java

--- src/main/java/com/hr/sb5mybatis/controller/test.java
+++ src/main/java/com/hr/sb5mybatis/controller/test.java
@@ -20,7 +20,7 @@ public class test {
 
     @RequestMapping("/queryAll")
     public ModelAndView queryAll(){
-
+        System.out.println("111");
         List<Person> list = ps.queryAll();
         ModelAndView view = new ModelAndView("/list");
         view.addObject("list",list);
